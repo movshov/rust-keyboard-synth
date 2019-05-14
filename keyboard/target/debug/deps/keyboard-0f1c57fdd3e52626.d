7b355fc935baa7d19224be41cbac8f9993e8179c
Bar Movshovich <barmovshovich@Bars-MacBook-Pro.local>
2019-05-13 18:35:02 -0700

    a

--- keyboard/target/debug/deps/keyboard-0f1c57fdd3e52626.d
+++
@@ -1,6 +0,0 @@
-/Users/barmovshovich/sound/keyboard/rust-keyboard-synth/keyboard/target/debug/deps/keyboard-0f1c57fdd3e52626: src/main.rs src/connection.rs
-
-/Users/barmovshovich/sound/keyboard/rust-keyboard-synth/keyboard/target/debug/deps/keyboard-0f1c57fdd3e52626.d: src/main.rs src/connection.rs
-
-src/main.rs:
-src/connection.rs:
--- keyboard/target/debug/deps/keyboard-2c8a1394704518db.d
+++
@@ -1,6 +0,0 @@
-/Users/barmovshovich/sound/keyboard/rust-keyboard-synth/keyboard/target/debug/deps/keyboard-2c8a1394704518db: src/main.rs src/connection.rs
-
-/Users/barmovshovich/sound/keyboard/rust-keyboard-synth/keyboard/target/debug/deps/keyboard-2c8a1394704518db.d: src/main.rs src/connection.rs
-
-src/main.rs:
-src/connection.rs:
